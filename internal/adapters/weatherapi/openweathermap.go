e4c1c27aaef94f1eee222c9e0dcfef0cf4d030e5
denis <denis.stitic@gmail.com>
2024-08-05 20:44:05 +0200

    Use API version 2.5

--- internal/adapters/weatherapi/openweathermap.go
+++ internal/adapters/weatherapi/openweathermap.go
@@ -15,12 +15,13 @@ type OpenWeatherMapAdapter struct {
 func NewOpenWeatherMapAdapter(apiKey string) *OpenWeatherMapAdapter {
 	return &OpenWeatherMapAdapter{
 		apiKey:  apiKey,
-		baseURL: "http://api.openweathermap.org/data/3.0/onecall",
+		baseURL: "https://api.openweathermap.org/data/2.5/weather",
 	}
 }
 
 func (a *OpenWeatherMapAdapter) FetchWeather(city string) (*domain.Weather, error) {
 	lattitude, longitude := getLatLonForCity(city)
+
 	url := fmt.Sprintf("%s?lat=%s&lon=%s&appid=%s&units=metric", a.baseURL, lattitude, longitude, a.apiKey)
 	resp, err := http.Get(url)
 	if err != nil {
@@ -33,25 +34,10 @@ func (a *OpenWeatherMapAdapter) FetchWeather(city string) (*domain.Weather, erro
 		return nil, err
 	}
 
-	current, ok := result["current"].(map[string]interface{})
-	if !ok {
-		return nil, fmt.Errorf("invalid response format")
-	}
-
-	weatherArray, ok := current["weather"].([]interface{})
-	if !ok || len(weatherArray) == 0 {
-		return nil, fmt.Errorf("weather information not found")
-	}
-
-	weatherInfo, ok := weatherArray[0].(map[string]interface{})
-	if !ok {
-		return nil, fmt.Errorf("invalid weather information format")
-	}
-
 	weather := &domain.Weather{
 		City:        city,
-		Temperature: current["temp"].(float64),
-		Description: weatherInfo["description"].(string),
+		Temperature: result["main"].(map[string]interface{})["temp"].(float64),
+		Description: result["weather"].([]interface{})[0].(map[string]interface{})["description"].(string),
 	}
 
 	return weather, nil
